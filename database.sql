dd2300103d8f9add40e169e3e2155a8f22765ce1
OnniVaananen <146675047+OnniVaananen@users.noreply.github.com>
2024-03-08 16:03:12 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,10 @@
 CREATE TABLE Tuotteet(
+  TuoteID INT AUTO_INCREMENT PRIMARY KEY,
+  NImi VACHAR(255) NOT NULL,
+  Valmistaja VACHAR(255),
+  Kuvaus TEXT,
+  Hinta DECIMAL(10, 2),
+  Varastosaldo INT,
+  EAN_koodi VACHAR(13) UNIQUE
+  Luontipaiva DATE
+  );
